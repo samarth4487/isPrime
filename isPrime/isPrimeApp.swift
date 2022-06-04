969fbd647f53c9eb343a72011905dcc14d76630d
samarth4487 <samarthpaboowal@yahoo.co.in>
2022-06-04 15:48:32 +0530

    Initial Commit

---
+++ isPrime/isPrimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  isPrimeApp.swift
+//  isPrime
+//
+//  Created by Samarth Paboowal on 04/06/22.
+//
+
+import SwiftUI
+
+@main
+struct isPrimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
